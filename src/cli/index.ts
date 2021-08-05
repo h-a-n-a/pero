05f9518019bac390abc449b26d85390aa9ec6e19
h-a-n-a <andywangsy@gmail.com>
2021-08-05 23:22:58 +0800

    fix: fix cli name

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -4,7 +4,7 @@ import { Command, Args, Compiler } from '../'
 
 export default (command: Command) => {
   command
-    .argument('<cli-name>', 'cli name')
+    .argument('<name>', 'cli name')
     .argument('[entry]', 'source cli directory path, default: \'src\'')
     // .option('-w, --watch', 'watch compilation')
     .option('-d, --dirPath <cli-dir>', 'source cli directory path')
@@ -16,14 +16,14 @@ export default (command: Command) => {
       return
     }
 
-    const { cliName, entry, dirPath, outDir } = args
+    const { name, entry, dirPath, outDir } = args
 
     const startTime = Date.now()
 
     const compiler = new Compiler({
       outDir: path.resolve(outDir || './dist'),
       root: path.resolve(entry || dirPath || './src'),
-      name: cliName
+      name
     })
 
     await compiler.compile()
