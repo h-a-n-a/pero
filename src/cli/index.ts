5aca0f8a7b782152568c02b7f609214e157221f8
wangshuyuan.h <wangshuyuan.h@bytedance.com>
2021-08-08 17:41:50 +0800

    feat: optimize cli

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -4,9 +4,9 @@ import { Command, Args, Compiler } from '../'
 
 export default (command: Command) => {
   command
-    .argument('<name>', 'cli name')
-    .argument('[entry]', 'source cli directory path, default: \'src\'')
+    .argument('<entry>', 'source cli directory path')
     // .option('-w, --watch', 'watch compilation')
+    .option('-n, --name <cli-name>', 'cli name')
     .option('-d, --dirPath <cli-dir>', 'source cli directory path')
     .option('-o, --outDir <out-dir>', 'output directory path, default: \'dist\'')
 
@@ -17,6 +17,11 @@ export default (command: Command) => {
     }
 
     const { name, entry, dirPath, outDir } = args
+
+    if (!name) {
+      console.log('cli name is required')
+      process.exit(1)
+    }
 
     const startTime = Date.now()
 
